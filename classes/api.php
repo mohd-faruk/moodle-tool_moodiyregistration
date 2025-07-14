f03fd469e4bf4aae829b61a82ec7f9825e385c15
pinky28 <pinky@vidyamantra.com>
2025-07-14 15:05:28 +0530

    Initial code commit

---
+++ classes/api.php
@@ -0,0 +1,162 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Class api for moodiy communication
+ *
+ * @package    tool_moodiyregistration
+ * @copyright  2025 VidyaMantra <pinky@vidyamantra.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace tool_moodiyregistration;
+defined('MOODLE_INTERNAL') || die();
+
+use moodle_exception;
+use curl;
+use stdClass;
+use coding_exception;
+use moodle_url;
+
+/**
+ * Provides methods to communicate with the hub (sites directory) web services.
+ *
+ * @package    tool_moodiyregistration
+ * @copyright  2025 VidyaMantra <pinky@vidyamantra.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class api {
+    /**
+     * The base URL for the Moodiy API.
+     *
+     * @var string
+     */
+    const API_URL = 'http://moodiy.local/api';
+
+    /**
+     * Register the site with Moodiy.
+     *
+     * @param array $params Parameters for registration.
+     * @return array Response from the API.
+     * @throws moodle_exception If there is an error during the API call.
+     */
+    public static function moodiy_registration($params = []) {
+        global $CFG;
+
+        $endpoint = self::API_URL . '/site/register';
+
+        $curl = new curl();
+        $header = ['Accept: application/json'];
+        $curl->setHeader($header);
+        $response = $curl->post($endpoint, $params);
+
+        $response = json_decode($response, true);
+        $info = $curl->get_info();
+        if ($curl->get_errno()) {
+            // Connection error.
+            throw new moodle_exception('errorconnect', 'tool_moodiyregistration', '', $curl->error);
+        } else if ($response === false) {
+            throw new coding_exception('Error calling API: ' . $curl->getError());
+        } else if ($info['http_code'] != 200 || empty($response['success'])) {
+            throw new moodle_exception($response['message'] ?? 'Error during registration');
+        } else {
+            return $response;
+        }
+    }
+
+    /**
+     * Update the registration information of the site.
+     *
+     * @param object $reginfo Registration information.
+     * @param array $params Parameters to update.
+     * @return array Response from the API.
+     * @throws moodle_exception If there is an error during the API call.
+     */
+    public static function update_registration(object $reginfo, array $params = []) {
+        global $CFG;
+
+        $endpoint = self::API_URL . '/site/register/'.$reginfo->registrationid;
+        try {
+            $payload = implode(',', $params);
+        } catch (\Exception $e) {
+            throw new moodle_exception( $e->getMessage());
+        }
+
+        $hmac = hash_hmac("sha256", $payload, $reginfo->site_uuid);
+
+        $curl = new curl();
+        $header = ['key: ' . $hmac];
+        $curl->setHeader($header);
+        // To fix laravel issue. Not parse the data if it's multipart/form-data(PUT/PATCH).
+        $params['_method'] = 'PUT';
+
+        $response = $curl->post($endpoint, $params);
+
+        $response = json_decode($response, true);
+
+        $info = $curl->get_info();
+        if ($curl->get_errno()) {
+            // Connection error.
+            throw new moodle_exception('errorconnect', 'tool_moodiyregistration', '', $curl->error);
+        } else if ($response === false) {
+            throw new coding_exception('Error calling API: ' . $curl->getError());
+        } else if ($info['http_code'] != 200 || empty($response['success'])) {
+            throw new moodle_exception($response['message']);
+        } else {
+            return $response;
+        }
+    }
+
+    /**
+     * Unregister the site from Moodiy.
+     *
+     * @param object $reginfo Registration information.
+     * @return array Response from the API.
+     * @throws moodle_exception If there is an error during the API call.
+     */
+    public static function unregister_site(object $reginfo) {
+        global $CFG;
+
+        $endpoint = self::API_URL . '/site/register/'.$reginfo->registrationid;
+        $params = [];
+        $params['site_uuid'] = $reginfo->site_uuid;
+        $params['timestamp'] = time();
+        $payload = implode(',', $params);
+        $hmac = hash_hmac("sha256", $payload, $reginfo->site_uuid);
+
+        $curl = new curl();
+        $header = ['key: ' . $hmac];
+        $curl->setHeader($header);
+        // To fix laravel issue. Not parse the data if it's multipart/form-data(PUT/PATCH).
+        $params['_method'] = 'DELETE';
+
+        $response = $curl->post($endpoint, $params);
+
+        $response = json_decode($response, true);
+        $info = $curl->get_info();
+
+        if ($curl->get_errno()) {
+            // Connection error.
+            throw new moodle_exception('errorconnect', 'tool_moodiyregistration', '', $curl->error);
+        } else if ($response === false) {
+            throw new coding_exception('Error calling API: ' . $curl->getError());
+        } else if ($info['http_code'] != 200 || empty($response['success'])) {
+            throw new moodle_exception($response['message']);
+        }
+        return $response;
+
+    }
+}
---
+++ lang/en/tool_moodiyregistration.php
@@ -0,0 +1,53 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     tool_moodiyregistration
+ * @category    string
+ * @copyright   2025 VidyaMantra <pinky@vidyamantra.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['errorregistrationupdate'] = 'An error occurred during registration update . Error: {$a}';
+$string['errorunregister'] = 'There was an error while unregistering your site. Error: {$a}';
+$string['eventmoodiyregistration'] = 'Site registered with Moodiy';
+$string['eventmoodiyregistrationpdated'] = 'Site registration updated with Moodiy';
+$string['eventmoodiyunregistration'] = 'Site unregistered from Moodiy';
+$string['pleaserefreshregistration'] = 'Your site is registered. Registration last updated {$a}.<br />Your registration will be kept up to date. You can also manually update your registration at any time.';
+$string['pleaserefreshregistrationunknown'] = 'Your site has been registered but the registration date is unknown. Please update your registration using the \'Update registration\' button.';
+$string['pluginname'] = 'Moodiy registration';
+$string['registerwithmoodiy'] = 'Register your site with Moodiy';
+$string['registerwithmoodiycomplete'] = 'Complete your site registration.';
+$string['registerwithmoodiyremove'] = 'You are about to unregister your site. You will no longer receive security alert notifications and users will not be able to receive push notifications from your site to their Moodle app. However, you will be able to re-register at any time! Are you sure you want to continue?';
+$string['registerwithmoodiyupdate'] = 'Update your site registration.';
+$string['registrationconfirmed'] = 'Site registration confirmed';
+$string['registrationconfirmedon'] = 'Thank you for registering your site. Registration information will be kept up to date by the scheduled task.';
+$string['registrationconfirmerror'] = 'There was an error confirming the registration. Please try again later.';
+$string['registrationerror'] = 'An error occurred while attempting to register the site: {$a}';
+$string['registrationerrorinvalidurl'] = 'The URL provided is invalid. Please check the URL and try again.';
+$string['registrationwarning'] = 'Don\'t miss out on important updates and security alerts.';
+$string['siteregistrationupdated'] = 'Site registration updated';
+$string['siteregistrationurlupdated'] = 'Site url updated with Moodiy.';
+$string['skipregistration'] = 'Skip registration';
+$string['tasksiteurlupdate'] = 'Site URL update with Moodiy';
+$string['unregister'] = 'Unregister';
+$string['unregistrationerror'] = 'An error occurred while attempting to unregister the site: {$a}';
+$string['updaterequest'] = 'Update request from Moodiy';
+$string['upgrade_monitor_task'] = 'Monitor core/plugins upgrade and update registration on Moodiy';
